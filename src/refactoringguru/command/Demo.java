c37c2c239523df48a5061e2107cfca92e3fe4dbc
Yousef Eldaly <yousefeldaly23@gmail.com>
2023-08-20 22:59:37 +0300

    Command pattern form refactoring guru

---
+++ src/refactoringguru/command/Demo.java
@@ -0,0 +1,10 @@
+package refactoringguru.command;
+
+import refactoringguru.command.editor.Editor;
+
+public class Demo {
+    public static void main(String[] args) {
+        Editor editor = new Editor();
+        editor.init();
+    }
+}
---
+++ src/refactoringguru/command/commands/CopyCommand.java
@@ -0,0 +1,16 @@
+package refactoringguru.command.commands;
+
+
+import refactoringguru.command.editor.Editor;
+
+public class CopyCommand extends Command {
+    public CopyCommand(Editor editor) {
+        super(editor);
+    }
+
+    @Override
+    public boolean execute() {
+        editor.clipboard = editor.textField.getSelectedText();
+        return false;
+    }
+}
